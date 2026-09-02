028043450f0fbe1836ba92084fc91225cb24fd68
Aspeternity <aspandre@163.com>
2026-09-03 07:52:06 +0800

    chore: show alpha.8.1 version

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -25,7 +25,7 @@ export function Sidebar() {
         </div>
         <div>
           <div className="font-semibold tracking-tight">SIMKeeper</div>
-          <div className="text-xs text-slate-400">v0.1.0-alpha.8</div>
+          <div className="text-xs text-slate-400">v0.1.0-alpha.8.1</div>
         </div>
       </div>
 
